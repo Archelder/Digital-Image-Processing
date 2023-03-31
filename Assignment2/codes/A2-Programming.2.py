8e5159348c88d0a0190fede08ec69eb900f6121e
Kilo A. FENG <Kilo_FENG@Outlook.com>
2023-03-31 16:31:32 +0800

    Update A2-Programming.2.py
    initially completed

--- Assignment2/codes/A2-Programming.2.py
+++ Assignment2/codes/A2-Programming.2.py
@@ -2,40 +2,59 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-LAPLACIAN_KERNEL = np.array([
-    [-1, -1, -1],
-    [-1, 8, -1],
-    [-1, -1, -1]
-],
-    dtype=np.int16)
-
-SOBEL_KERNEL = np.array([
-    [-1, -2, -1],
-    [0, 0, 0],
-    [1, 2, 1],
-],
-    dtype=np.int16)
-
 a_original_img = cv2.imread('../images/Fig0343(a)(skeleton_orig).png', 0)
-a_original_img = a_original_img.astype(np.int16)
+a_original_img = a_original_img.astype(np.float64)
 
-b_laplacian_img = cv2.filter2D(a_original_img, -1, LAPLACIAN_KERNEL)
+b_laplacian_img = cv2.Laplacian(a_original_img, cv2.CV_64F, None)
+norm_b_laplacian_img = cv2.normalize(b_laplacian_img, None, 0, 255, cv2.NORM_MINMAX)
 
 c_sharpened_img = cv2.add(a_original_img, b_laplacian_img)  # c_sharpened_img = a_original_img + b_laplacian_img
 
-d_sobel_img = cv2.Sobel(a_original_img,-1)
+d_sobel_img_x = cv2.Sobel(a_original_img, cv2.CV_64F, 1, 0)
+d_sobel_img_x = cv2.convertScaleAbs(d_sobel_img_x)
+d_sobel_img_y = cv2.Sobel(a_original_img, cv2.CV_64F, 0, 1)
+d_sobel_img_y = cv2.convertScaleAbs(d_sobel_img_y)
+d_sobel_img = cv2.addWeighted(d_sobel_img_x, 0.5, d_sobel_img_y, 0.5, 0, dtype=cv2.CV_64F)
 
-e_smoothed_sobel_img = None
-f_mask_img = None  # f_mask_img = b_laplacian_img * e_smoothed_sobel_img
-g_sharpened_img = None  #
-h_sigma_trans_img = None  # transformed from g_sharpened_img
+e_smoothed_sobel_img = cv2.blur(d_sobel_img, (5, 5))
 
-norm_b_laplacian_img = cv2.normalize(b_laplacian_img, None, 0, 255, cv2.NORM_MINMAX)
+f_mask_img = np.multiply(norm_b_laplacian_img, e_smoothed_sobel_img)
+# f_mask_img = cv2.convertScaleAbs(f_mask_img)
+# f_mask_img = f_mask_img.astype(np.uint8)
+# f_mask_img = cv2.normalize(f_mask_img, None, 0, 256, cv2.NORM_MINMAX)
 
-plt.figure(figsize=(10, 8))
-plt.subplot(221), plt.imshow(a_original_img, cmap='gray'), plt.title('original'), plt.axis('off')
-plt.subplot(222), plt.imshow(b_laplacian_img, cmap='gray'), plt.title('laplacian'), plt.axis('off')
-plt.subplot(223), plt.imshow(c_sharpened_img, cmap='gray'), plt.title('sharpened by laplacian'), plt.axis('off')
-plt.subplot(224), plt.imshow(d_sobel_img, cmap='gray'), plt.title('sobel grad'), plt.axis('off')
+g_sharpened_img = cv2.add(a_original_img, f_mask_img)
+h_gamma_trans_img = (cv2.pow((g_sharpened_img / 255), 0.5)) * 255  # transformed from g_sharpened_img
+
+plt.figure(figsize=(12, 8))
+
+ls = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+i = 1
+plt.subplot(2, 4, i)
+plt.imshow(a_original_img, cmap='gray'), plt.title(f'({ls[i - 1]}) original'), plt.axis('off')
+i += 1
+plt.subplot(2, 4, i)
+plt.imshow(norm_b_laplacian_img, cmap='gray'), plt.title(f'({ls[i - 1]}) laplacian'), plt.axis('off')
+i += 1
+plt.subplot(2, 4, i)
+plt.imshow(c_sharpened_img, cmap='gray'), plt.title(f'({ls[i - 1]}) sharpened by laplacian'), plt.axis('off')
+i += 1
+plt.subplot(2, 4, i)
+plt.imshow(d_sobel_img, cmap='gray'), plt.title(f'({ls[i - 1]}) sobel'), plt.axis('off')
+i += 1
+plt.subplot(2, 4, i)
+plt.imshow(e_smoothed_sobel_img, cmap='gray'), plt.title(f'({ls[i - 1]}) smoothed sobel'), plt.axis('off')
+i += 1
+plt.subplot(2, 4, i)
+plt.imshow(f_mask_img, cmap='gray'), plt.title(f'({ls[i - 1]}) mask'), plt.axis('off')
+i += 1
+plt.subplot(2, 4, i)
+plt.imshow(g_sharpened_img, cmap='gray'), plt.title(f'({ls[i - 1]}) sharpened by mask'), plt.axis('off')
+i += 1
+plt.subplot(2, 4, i), plt.imshow(h_gamma_trans_img, cmap='gray')
+plt.title(f'({ls[i - 1]}) sharpened by mask\n ($\gamma = 0.7$)'), plt.axis('off')
+
+# cv2.imshow('img f', f_mask_img)
+# cv2.waitKey(0)
 plt.tight_layout()
 plt.show()
