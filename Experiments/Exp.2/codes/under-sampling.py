ce60f067ff131853aa37c570457bdf46d64ccc30
Kilo A. FENG <Kilo_FENG@Outlook.com>
2023-04-22 09:58:24 +0800

    Finish under-sampling

--- Experiments/Exp.2/codes/under-sampling.py
+++ Experiments/Exp.2/codes/under-sampling.py
@@ -1,4 +1,21 @@
 import cv2
 import matplotlib.pyplot as plt
 
-original_img = cv2.imread("images/Fig0220(a)(chronometer 3692x2812  2pt25 inch 1250 dpi).tif")
+original_img = cv2.imread("../images/Fig0220(a)(chronometer 3692x2812  2pt25 inch 1250 dpi).tif", flags=0)
+ORIGINAL_DPI = 1250
+target_dpi = [300, 150, 72]
+
+resize_ratio = []
+for i in target_dpi:
+    resize_ratio.append((i / ORIGINAL_DPI))
+
+target_img = []
+for ratio in resize_ratio:
+    target_img.append(cv2.resize(original_img, dsize=None, fx=ratio, fy=ratio))
+
+plt.subplot(221), plt.imshow(original_img, cmap='gray'), plt.title("1250 dpi"), plt.axis('off')
+
+for i in range(3):
+    plt.subplot(222 + i), plt.imshow(target_img[i], cmap='gray'), plt.title(f"{target_dpi[i]} dpi"), plt.axis('off')
+
+plt.show()
