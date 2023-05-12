df9f235e7eb1ac461a4a088101af701701e39264
kurarinetto <2114621614@qq.com>
2023-05-12 11:40:48 +0800

    Update A4-programing1.py

--- Assignment4/codes/A4-programing1.py
+++ Assignment4/codes/A4-programing1.py
@@ -2,3 +2,19 @@ import cv2
 import matplotlib.pyplot as plt
 
 #open
+original_image=cv2.imread("../images/FigP0501.png",flags=0)
+size=(3,3)
+
+target_image=cv2.blur(original_image,size)
+
+fig, axs = plt.subplots(nrows=1, ncols=1, figsize=(15, 15))
+
+ax = axs[0, 0]
+ax.imshow(target_image, cmap='gray')
+ax.set_title(f"3*3")
+ax.set_xticks([])
+ax.set_yticks([])
+
+plt.tight_layout()
+
+plt.show()
