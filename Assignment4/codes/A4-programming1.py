4ee2448405b2f03081d5e04bc8de59f55a02e951
Kilo A. FENG <Kilo_FENG@Outlook.com>
2023-05-12 12:12:38 +0800

    Update A4-programming1.py

--- Assignment4/codes/A4-programming1.py
+++ Assignment4/codes/A4-programming1.py
@@ -6,7 +6,7 @@ original_image = cv2.imread("../images/FigP0501.png", flags=0)
 size1 = (3, 3)
 size2 = (7, 7)
 size3 = (9, 9)
-size = ["3*3", "7*7", "9*9"]
+size = [3, 7, 9]
 
 target_image = []
 target_image.append(cv2.blur(original_image, size1))
@@ -14,8 +14,7 @@ target_image.append(cv2.blur(original_image, size2))
 target_image.append(cv2.blur(original_image, size3))
 
 # show
-fig, axs = plt.subplots(nrows=1, ncols=4, figsize=(10, 5))
-
+fig, axs = plt.subplots(nrows=1, ncols=4, figsize=(10, 4))
 
 ax = axs[0]
 ax.imshow(original_image, cmap='gray')
@@ -24,13 +23,13 @@ ax.set_xticks([])
 ax.set_yticks([])
 
 for i in range(3):
-    ax = axs[i+1]
+    ax = axs[i + 1]
     ax.imshow(target_image[i], cmap='gray')
-    ax.set_title(f"{size[i]}")
+    ax.set_title(fr"${size[i]}\times${size[i]}")
     ax.set_xticks([])
     ax.set_yticks([])
 
-plt.suptitle(f"Running Results of 1")
+plt.suptitle("Running Results of Arithmetic Mean Filtering")
 
 plt.tight_layout()
 
