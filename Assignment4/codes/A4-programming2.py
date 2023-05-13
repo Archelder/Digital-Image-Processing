8dd9671043042a831fa1b74a14f1e1600ab8577d
Kilo A. FENG <Kilo_FENG@Outlook.com>
2023-05-13 14:14:20 +0800

    Update A4-programming2.py
    done

--- Assignment4/codes/A4-programming2.py
+++ Assignment4/codes/A4-programming2.py
@@ -3,16 +3,46 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def geometric_mean_filter(img, size):
+def geometric_mean_filter(img, ksize):
     h, w = img.shape[:2]
-    order = 1 / np.power(size, 2)
-    pad = int((size - 1) / 2)
-    padded_img = np.pad(img, pad, mode='edge')
+    expo = 1 / (ksize * ksize)
+    pad = int((ksize - 1) / 2)
+    padded = cv2.copyMakeBorder(img, pad, pad, pad, pad, borderType=cv2.BORDER_REFLECT_101)
+    filtered = np.zeros(img.shape)
+    for i in range(pad, pad + h):
+        for j in range(pad, pad + w):
+            prod = np.prod(padded[i - pad:i + pad, j - pad:j + pad])
+            filtered[i - pad][j - pad] = np.power(prod, expo)
+    return filtered.astype(np.uint8)
 
 
-def GMF(shape):
-    return geometric_mean_filter(shape)
+def GMF(img, ksize):
+    return geometric_mean_filter(img, ksize)
 
+
+kernel_size = [3, 7, 9]
 
 # read the original image
 original_image = cv2.imread("../images/FigP0501.png", flags=0)
+
+filtered_img = []
+
+# apply filters with different kernel sizes respectively
+for ksize in kernel_size:
+    filtered_img.append(GMF(original_image, ksize))
+
+# display the results
+fig, axs = plt.subplots(nrows=1, ncols=4, figsize=(10, 4))
+ax = axs[0]
+ax.imshow(original_image, cmap='gray'), ax.set_title(f"original image"), ax.set_xticks([]), ax.set_yticks([])
+for i in range(3):
+    ax = axs[i + 1]
+    ax.imshow(filtered_img[i], 'gray')
+    ax.set_title(fr"${kernel_size[i]}\times${kernel_size[i]}")
+    ax.set_xticks([]), ax.set_yticks([])
+
+plt.suptitle("Running Results of Geometric Mean Filtering")
+
+plt.tight_layout()
+
+plt.show()
