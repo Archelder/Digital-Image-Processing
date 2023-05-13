6af02216441269a942fdc7c7c4fe2834125a0c65
Kilo A. FENG <Kilo_FENG@Outlook.com>
2023-05-13 14:39:59 +0800

    Optimise captions for fig. and filter fuction

--- Assignment4/codes/A4-programming2.py
+++ Assignment4/codes/A4-programming2.py
@@ -7,13 +7,14 @@ def geometric_mean_filter(img, ksize):
     h, w = img.shape[:2]
     expo = 1 / (ksize * ksize)
     pad = int((ksize - 1) / 2)
+    # pad the image using `cv2.copyMakeBorder()` whose effect can also be produced by`np.pad()`
     padded = cv2.copyMakeBorder(img, pad, pad, pad, pad, borderType=cv2.BORDER_REFLECT_101)
     filtered = np.zeros(img.shape)
     for i in range(pad, pad + h):
         for j in range(pad, pad + w):
             prod = np.prod(padded[i - pad:i + pad, j - pad:j + pad])
             filtered[i - pad][j - pad] = np.power(prod, expo)
-    return filtered.astype(np.uint8)
+    return filtered
 
 
 def GMF(img, ksize):
@@ -38,7 +39,7 @@ ax.imshow(original_image, cmap='gray'), ax.set_title(f"original image"), ax.set_
 for i in range(3):
     ax = axs[i + 1]
     ax.imshow(filtered_img[i], 'gray')
-    ax.set_title(fr"${kernel_size[i]}\times${kernel_size[i]}")
+    ax.set_title(fr"${kernel_size[i]}\times${kernel_size[i]} GMF")
     ax.set_xticks([]), ax.set_yticks([])
 
 plt.suptitle("Running Results of Geometric Mean Filtering")
