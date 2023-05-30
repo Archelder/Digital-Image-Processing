d63d11ce8162de14a6c7fabef0c585652bcb0a5a
Kilo A. FENG <Kilo_FENG@Outlook.com>
2023-05-30 17:01:11 +0800

    Update A5-prog3_thresholding_segment.py

--- Assignment5/codes/A5-prog3_thresholding_segment.py
+++ Assignment5/codes/A5-prog3_thresholding_segment.py
@@ -2,4 +2,14 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-original_image = cv2.imread("../images/FigP1036.png", 0)
+original_img = cv2.imread("../images/FigP1036.png", 0)
+thresholded_img = cv2.threshold(original_img, 60, 255, cv2.THRESH_OTSU)[1]
+
+plt.figure(figsize=(8, 6))
+plt.subplot(221),plt.imshow(original_img, 'gray'), plt.title("original")
+
+plt.subplot(222),plt.imshow(thresholded_img, 'gray'), plt.title("thresholded")
+
+plt.subplot(212),plt.hist(original_img.flatten(), 256, [0, 255],log=True)
+plt.tight_layout()
+plt.show()
